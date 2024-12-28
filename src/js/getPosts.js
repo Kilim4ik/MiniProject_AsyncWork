e0f39b9602ffdea3e16c5c9486cec9bbb43c7065
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-12-28 13:07:31 +0200

    add limit for pagination

--- src/js/getPosts.js
+++ src/js/getPosts.js
@@ -2,7 +2,7 @@
 const baseURL = 'http://localhost:3000';
 async function getPosts(page) {
   try {
-    return fetch(`${baseURL}&page=${page}`).then(res => res.json());
+    return fetch(`${baseURL}?_page=${page}&_limit=10`).then(res => res.json());
   } catch (error) {
     console.error(error);
   }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import renderPosts from "renderPosts.js";
 async function startApp() {
-  const posts = await getPosts();
+  const posts = await getPosts(1);
   renderPosts(posts);
 }
 startApp();
